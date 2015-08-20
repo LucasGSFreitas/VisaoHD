43862368c7b2c1672af02b099b909e6d0b9e6aec
giulianaSA <giulialeproti@hotmail.com>
2015-08-20 16:06:53 -0300

    Animações

--- MiniChallange2/MiniChallange2/SceneBadminton.swift
+++ MiniChallange2/MiniChallange2/SceneBadminton.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class SceneBadminton: UIViewController {
     
+    let turn:Bool = false
+    let eyePosition = CGFloat(600.0)
+    
     let imageNamedPeteca = "petecaB"
     let imageNamedRaquete = "raqueteBadminton"
 
@@ -24,6 +27,8 @@ class SceneBadminton: UIViewController {
         let imageR = UIImage(named:imageNamedRaquete)
         imageRaquete.image = imageR
 
+        saque()
+        
         
     }
     
@@ -31,9 +36,47 @@ class SceneBadminton: UIViewController {
 
     func saque()
     {
+        //Make rotation over all transformations already done
+        var rotationAndPerspectiveTransform = imageRaquete.layer.transform
+        
+        //Create the perspective effect. The m34 parameter changes the distance from
+        //the layer to the projection plane.
+        //Reference: https://en.wikipedia.org/wiki/3D_projection#Perspective_projection
+        rotationAndPerspectiveTransform.m34 = -1.0 / eyePosition
+        
+        if (!turn) {
+            
+            rotationAndPerspectiveTransform = CATransform3DRotate(rotationAndPerspectiveTransform, CGFloat(M_PI * 0.3), CGFloat(0.0), CGFloat(-1.0), CGFloat(0.0));
+
+            rotationAndPerspectiveTransform = CATransform3DRotate(rotationAndPerspectiveTransform, CGFloat(M_PI * 0.4), CGFloat(-1.0), CGFloat(-1.0), CGFloat(0.0));
+            
+            rotationAndPerspectiveTransform = CATransform3DRotate(rotationAndPerspectiveTransform, CGFloat(M_PI * 0.2), CGFloat(-1.0), CGFloat(0.0), CGFloat(-1.0));
+
+            
+        } else {
+            rotationAndPerspectiveTransform = CATransform3DRotate(rotationAndPerspectiveTransform, CGFloat(0.0), CGFloat(0.0), CGFloat(0.0), CGFloat(0.0));
+        }
+        
+//        rotationAndPerspectiveTransform = CATransform3DRotate(rotationAndPerspectiveTransform, CGFloat(M_PI * 0.3), CGFloat(0.0), CGFloat(-1.0), CGFloat(0.0));
+      rotateTopImage(rotationAndPerspectiveTransform)
+        
+   //     turn = !turn
+        
+    }
+    
+    func rotateTopImage(transform:CATransform3D) {
+        
+        let animation = CABasicAnimation(keyPath: "transform")
+        animation.fillMode = kCAFillModeForwards
+        animation.removedOnCompletion = false
+        animation.duration = 1.0
+        animation.toValue = NSValue(CATransform3D: transform)
+        
+        self.imageRaquete.layer.addAnimation(animation, forKey: nil)
         
         
     }
+
     
     
     
