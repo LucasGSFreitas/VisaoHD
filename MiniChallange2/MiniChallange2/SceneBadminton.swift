b90a888cf7076708c74309ed46853c7f1f1c4f3e
giulianaSA <giulialeproti@hotmail.com>
2015-08-26 15:46:20 -0300

    Mais animação badminton

--- MiniChallange2/MiniChallange2/SceneBadminton.swift
+++ MiniChallange2/MiniChallange2/SceneBadminton.swift
@@ -39,24 +39,27 @@ class SceneBadminton: UIViewController {
     
         
  
-       // saque()
         
     }
     
     
     @IBAction func Start(sender: AnyObject) {
-        saque()
+        
+        self.saqueRaquete()
+        
     }
+    
+  
 
-    func saque()
+    func saqueRaquete()
     {
         //Make rotation over all transformations already done
         var rotationAndPerspectiveTransform = imageRaquete.layer.transform
-        var transladeAndScaleTransform = imagePeteca.layer.transform
+        
         
         
         //Reference: https://en.wikipedia.org/wiki/3D_projection#Perspective_projection
-        rotationAndPerspectiveTransform.m34 = -1.0 / eyePosition
+        rotationAndPerspectiveTransform.m34 = 1.0 / eyePosition
         
         //Para rotacionar a raquete:
         rotationAndPerspectiveTransform = CATransform3DRotate(rotationAndPerspectiveTransform, CGFloat(M_PI * 0.55), CGFloat(0.0), CGFloat(-1.0), CGFloat(0.0));
@@ -67,24 +70,40 @@ class SceneBadminton: UIViewController {
         
         rotateRaqueteImage(rotationAndPerspectiveTransform)
         
-        //Para fazer a peteca ir para o outro lado da rede.
+        
+    }
+    
+   func saquePeteca()
+    {
+        var transladeAndScaleTransform = imagePeteca.layer.transform
+    
+            //Para fazer a peteca ir para o outro lado da rede.
     
         transladeAndScaleTransform = CATransform3DTranslate(transladeAndScaleTransform, 20, -250, 0)
          rotatePetecaImage(transladeAndScaleTransform)
+        self.imagePeteca.layer.zPosition = 2
+
         transladeAndScaleTransform = CATransform3DTranslate(transladeAndScaleTransform, 50, -10, 0)
         rotatePetecaImage(transladeAndScaleTransform)
-        transladeAndScaleTransform = CATransform3DTranslate(transladeAndScaleTransform, 50, 10, 0)
+       // self.imagePeteca.layer.zPosition = -2
+
+        transladeAndScaleTransform = CATransform3DTranslate(transladeAndScaleTransform, 70, 10, 0)
          rotatePetecaImage(transladeAndScaleTransform)
-        transladeAndScaleTransform = CATransform3DTranslate(transladeAndScaleTransform, 30, 160, 0)
+        transladeAndScaleTransform = CATransform3DTranslate(transladeAndScaleTransform, 10, 160, 0)
         
-        
+
         
         transladeAndScaleTransform = CATransform3DScale(transladeAndScaleTransform, 0.5, 0.5, 1)
         
         rotatePetecaImage(transladeAndScaleTransform)
-        
+
     }
     
+    
+    override func animationDidStop(anim: CAAnimation!, finished flag: Bool)
+    {
+        self.saquePeteca()
+    }
 
     
     func rotateRaqueteImage(transform:CATransform3D) {
@@ -94,6 +113,9 @@ class SceneBadminton: UIViewController {
         animation.removedOnCompletion = false
         animation.duration = 1.0
         animation.toValue = NSValue(CATransform3D: transform)
+        animation.delegate = self
+        
+        self.imageQuadra.layer.zPosition = -1000
         
         self.imageRaquete.layer.addAnimation(animation, forKey: nil)
         
@@ -108,6 +130,8 @@ class SceneBadminton: UIViewController {
         animation.duration = 2.0
         animation.toValue = NSValue(CATransform3D: transform)
         
+        self.imageQuadra.layer.zPosition = -1000
+
         self.imagePeteca.layer.addAnimation(animation, forKey: nil)
         
         
